a8fb8cd318edf20771a15dc1aa560e8d5d892ca7
Kim Yoo <kimyoo04eco@naver.com>
2022-08-18 02:30:13 +0900

    #5.16 Selectors part start

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import {ThemeProvider} from "styled-components";
 import {darkTheme} from "./theme";
+import {RecoilRoot} from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
